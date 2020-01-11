e7a2513be33ad961b7650b9e2805d71b4a5a3bb5
ShadhiN <shadhinemail@gmail.com>
2020-01-11 23:08:49 +0600

    just video play

---
+++ app/src/main/java/com/era/bongo/VideoPlayerActivity.java
@@ -0,0 +1,28 @@
+package com.era.bongo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+        VideoView videoView =(VideoView)findViewById(R.id.vdVw);
+        //Set MediaController  to enable play, pause, forward, etc options.
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        //Location of Media File
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.a);
+        //Starting VideView By Setting MediaController and URI
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+}}
